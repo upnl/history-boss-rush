da80b5295c9d116b355363d4468f226a72454bd0
yoonchulroh <yoonchulroh@gmail.com>
2023-07-16 00:58:21 +0900

    bottle now changes according to the amount of blood left

---
+++ Assets/Scripts/StoreSceneScripts/BottleBehaviour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleBehaviour : MonoBehaviour
+{
+    private float _startBlood;
+
+    [SerializeField] private Sprite _fullBottle;
+    [SerializeField] private Sprite _halfBottle;
+    [SerializeField] private Sprite _emptyBottle;
+    void Start()
+    {
+        _startBlood = BookManager.Instance.blood;
+        GetComponent<SpriteRenderer>().sprite = _fullBottle;
+    }
+
+    void Update()
+    {
+        if (BookManager.Instance.blood <= _startBlood * 0.25)
+        {
+            GetComponent<SpriteRenderer>().sprite = _emptyBottle;
+        } else if (BookManager.Instance.blood <= _startBlood * 0.75)
+        {
+            GetComponent<SpriteRenderer>().sprite = _halfBottle;
+        }
+    }
+}
