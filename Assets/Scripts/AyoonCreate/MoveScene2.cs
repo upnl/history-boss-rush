545a31f35c525075277d016dfc8eeb8ec3e5ee0c
imanoony <93924580+imanoony@users.noreply.github.com>
2023-08-24 14:31:14 +0900

    필드맵 텍스트 호버 추가

--- Assets/Scripts/AyoonCreate/MoveScene2.cs
+++ Assets/Scripts/AyoonCreate/MoveScene2.cs
@@ -7,10 +7,12 @@ public class MoveScene2 : MonoBehaviour
 {
     public string sceneName = "";
 
+    [SerializeField] public GameObject name;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        name.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class MoveScene2 : MonoBehaviour
     private void OnMouseOver()
     {
         gameObject.GetComponent<SpriteRenderer>().material.color = new Color(169 / 255f, 169 / 255f, 169 / 255f, 255 / 255f);
+        name.gameObject.SetActive(true);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -32,5 +35,6 @@ public class MoveScene2 : MonoBehaviour
     private void OnMouseExit()
     {
         gameObject.GetComponent<SpriteRenderer>().material.color = Color.white;
+        name.gameObject.SetActive(false);
     }
 }
