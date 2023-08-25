9ddb9b3c22e5cd74cdc2febbbdc6c9f8798ff4c8
imanoony <93924580+imanoony@users.noreply.github.com>
2023-08-25 17:01:08 +0900

    팝업 추가

--- Assets/Scripts/AyoonCreate/PopUIManager.cs
+++ Assets/Scripts/AyoonCreate/PopUIManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PopUIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static PopUIManager Instance { get; private set; }
+
+    public TextMeshProUGUI textObj;
+
+    public string nowObj = "";
+
+    void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        HidePopUI();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ShowPopUI(string text)
+    {
+        gameObject.SetActive(true);
+        textObj.text = text;
+    }
+
+    public void HidePopUI()
+    {
+        gameObject.SetActive(false);
+        nowObj = "";
+        textObj.text = "";
+    }
+
+    public void Move()
+    {
+        SceneManager.LoadScene(nowObj);
     }
 }
