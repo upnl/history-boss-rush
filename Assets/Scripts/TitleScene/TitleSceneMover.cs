dc907e6561cef1e629c43187feeb9954eebb8a68
shin005004 <39648620+shin005004@users.noreply.github.com>
2023-08-27 03:24:24 +0900

    튜토리얼 추가

--- Assets/Scripts/TitleScene/TitleSceneMover.cs
+++ Assets/Scripts/TitleScene/TitleSceneMover.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class TitleSceneMover : MonoBehaviour
 {
+    public GameObject Tutorial1;
+    public GameObject Tutorial2;
+
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene("StoreScene");
+            if (Tutorial1.activeSelf == false)
+            {
+                Tutorial1.SetActive(true);
+            }
+            else
+            {
+                if (Tutorial2.activeSelf == false)
+                {
+                    Tutorial2.SetActive(true);
+                }
+                else
+                {
+                    SceneManager.LoadScene("StoreScene");
+                }
+            }
         }
     }
 }
