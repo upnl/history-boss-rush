bef18d2192f982bd837d9daaa3d16b5f2eae4db2
jhoh0518 <jhoh0518@gmail.com>
2024-01-06 15:33:18 +0900

    Add portal objects

--- Assets/Scripts/StoreCameraController.cs
+++ Assets/Scripts/StoreCameraController.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class StoreCameraController : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject player;
+
+    private void Update() {
+        mainCamera.transform.position = player.transform.position + new Vector3(0f, 0f, -10);
+    }
 }
