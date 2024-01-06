49b24a3351eaefcde608ad2f4e7cf06d9f1f0477
jhoh0518 <129178006+jhoh0518@users.noreply.github.com>
2024-01-06 16:10:45 +0900

    Add loading scene

--- Assets/Scripts/StoreSceneScripts/StorePortal.cs
+++ Assets/Scripts/StoreSceneScripts/StorePortal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class StorePortal : MonoBehaviour
@@ -14,29 +15,40 @@ public class StorePortal : MonoBehaviour
                 switch(destination){
                     case "Passive":
                         BookManager.Instance.SetRoomPassive();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "Europe":
                         BookManager.Instance.SetRoomEurope();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "Asia":
                         BookManager.Instance.SetRoomAsia();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "NorthAmerica":
                         BookManager.Instance.SetRoomNorthAmerica();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "SouthAmerica":
                         BookManager.Instance.SetRoomSouthAmerica();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "Africa":
                         BookManager.Instance.SetRoomAfrica();
+                        SceneLoader.Instance.LoadScene("StoreScene");
                         break;
                     case "Australia":
                         BookManager.Instance.SetRoomAustralia();
+                        SceneLoader.Instance.LoadScene("StoreScene");
+                        break;
+                    case "WorldMap":
+                        SceneLoader.Instance.LoadScene("FieldScene");
                         break;
                     default:
                         Debug.Log("Destination is wrongly set!");
                         break;
                 }
+                
             }
         }
     }
