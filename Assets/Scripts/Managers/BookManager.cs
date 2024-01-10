68df83c35e9bb5080d362fa35767470a5be3bc12
jhoh0518 <129178006+jhoh0518@users.noreply.github.com>
2024-01-10 23:17:25 +0900

    Fixing Structure

--- Assets/Scripts/Managers/BookManager.cs
+++ Assets/Scripts/Managers/BookManager.cs
@@ -9,7 +9,9 @@ public class BookManager : MonoBehaviour
     public string roomTypeSetting;
 
 #region Book List Declaration
-    public List<string> TotalBookList = new List<string>();
+    public List<string> PassiveList = new List<string>{};
+
+    public List<string> TotalBookList = new List<string> {};
     public Dictionary<string, List<string>> PassiveBookList = new Dictionary<string, List<string>>();
     public Dictionary<string, List<string>> EuropeBookList = new Dictionary<string, List<string>>();
     public Dictionary<string, List<string>> AsiaBookList = new Dictionary<string, List<string>>();
@@ -17,6 +19,17 @@ public class BookManager : MonoBehaviour
     public Dictionary<string, List<string>> SouthAmericaBookList = new Dictionary<string, List<string>>();
     public Dictionary<string, List<string>> AfricaBookList = new Dictionary<string, List<string>>();
     public Dictionary<string, List<string>> AustraliaBookList = new Dictionary<string, List<string>>();
+#endregion
+
+#region Boss List Declaration
+    [Header("Boss List")]
+    public List<string> TotalBoss = new List<string> {};
+    public List<string> EuropeBoss = new List<string>();
+    public List<string> AsiaBoss = new List<string>();
+    public List<string> NorthAmericaBoss = new List<string>();
+    public List<string> SouthAmericaBoss = new List<string>();
+    public List<string> AfricaBoss = new List<string>();
+    public List<string> AustraliaBoss = new List<string>();
 #endregion
 
     private BookData _bookData = new BookData();
@@ -28,21 +41,16 @@ public class BookManager : MonoBehaviour
     public CSVReader dialogueDB;
     public int iteration;
 
-    private bool _thorDefeated = false;
-    private bool _surtrDefeated = false;
-    private bool _hellDefeated = false;
-    private bool _lokiDefeated = false;
-
-    public bool thorDefeated => _thorDefeated;
-    public bool surtrDefeated => _surtrDefeated;
-    public bool hellDefeated => _hellDefeated;
-    public bool lokiDefeated => _lokiDefeated;
+    private Dictionary<string, bool> _bossDefeated = new Dictionary<string, bool>();
+    public Dictionary<string, bool> bossDefeated => _bossDefeated;
 
     [SerializeField] private TextAsset _bookDB;
     [SerializeField] private TextAsset _dialogueDB;
 
     public string bookDescription;
 
+
+#region Initiate
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -54,10 +62,9 @@ public class BookManager : MonoBehaviour
         Instance = this;
 
         DontDestroyOnLoad(this.gameObject);
-        TotalBookList = new List<string> {"Surtr1", "Surtr2", "Surtr3", "Surtr4", "Surtr5", "Thor1", "Thor2", "Thor3", "Thor4"};
 
-        EuropeBookList["Surtr"] = new List<string> {"Surtr1", "Surtr2", "Surtr3", "Surtr4", "Surtr5"};
-        EuropeBookList["Thor"] = new List<string> {"Thor1", "Thor2", "Thor3", "Thor4"};
+        initiateBookList();
+        initiateBossDefeated();
 
         bookDB = new CSVReader(_bookDB, true, '\t');
         dialogueDB = new CSVReader(_dialogueDB, true, ',');
@@ -65,6 +72,107 @@ public class BookManager : MonoBehaviour
 
         bookDescription = "";
     }
+
+    private int i, j;
+    private string iStr, jStr, bookName;
+    private void initiateBookList(){
+        foreach (string Boss in EuropeBoss){
+            TotalBoss.Add(Boss);
+            EuropeBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    EuropeBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Boss in AsiaBoss){
+            TotalBoss.Add(Boss);
+            AsiaBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    AsiaBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Boss in NorthAmericaBoss){
+            TotalBoss.Add(Boss);
+            NorthAmericaBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    NorthAmericaBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Boss in SouthAmericaBoss){
+            TotalBoss.Add(Boss);
+            SouthAmericaBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    SouthAmericaBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Boss in AfricaBoss){
+            TotalBoss.Add(Boss);
+            AfricaBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    AfricaBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Boss in AustraliaBoss){
+            TotalBoss.Add(Boss);
+            AustraliaBookList[Boss] = new List<string> {};
+            for(i = 1; i <= 4; i++){
+                for(j = 1; j <= 2; j++){
+                    iStr = i.ToString();
+                    jStr = j.ToString();
+                    bookName = $"{Boss} {iStr}-{jStr}";
+                    AustraliaBookList[Boss].Add(bookName);
+                    TotalBookList.Add(bookName);
+                } 
+            }
+        }
+        foreach (string Passive in PassiveList){
+            PassiveBookList[Passive] = new List<string> {};
+            for(i = 1; i <= 2; i++){
+                iStr = i.ToString();
+                bookName = $"{Passive} {iStr}";
+                PassiveBookList[Passive].Add(bookName);
+                TotalBookList.Add(bookName);
+            }
+        }
+
+    }
+
+    private void initiateBossDefeated(){
+        foreach (string Boss in TotalBoss){
+            _bossDefeated[Boss] = false;
+        }
+    }
+#endregion
+
 
 #region Room Setting
     public void SetRoomPassive(){
@@ -196,85 +304,13 @@ public class BookManager : MonoBehaviour
 #region Boss Defeat
     public void BossDefeated(string bossName)
     {
-        if (bossName == "Thor")
-        {
-            _thorDefeated = true;
-        }
-
-        if (bossName == "Surtr")
-        {
-            _surtrDefeated = true;
-        }
-
-        if (bossName == "Hell")
-        {
-            _hellDefeated = true;
-        }
-
-        if (bossName == "Loki")
-        {
-            _lokiDefeated = true;
-        }
+        _bossDefeated[bossName] = true;
     }
 
     public int CheckBossDefeated(string bossName)
     {
-        if (bossName == "Thor")
-        {
-            if (thorDefeated)
-            {
-                return 1;
-            }
-
-            else
-            {
-                return 0;
-            }
-        }
-
-        else if (bossName == "Surtr")
-        {
-            if (surtrDefeated)
-            {
-                return 1;
-            }
-
-            else
-            {
-                return 0;
-            }
-        }
-
-        else if (bossName == "Hell")
-        {
-            if (hellDefeated)
-            {
-                return 1;
-            }
-
-            else
-            {
-                return 0;
-            }
-        }
-
-        else if (bossName == "Loki")
-        {
-            if (lokiDefeated)
-            {
-                return 1;
-            }
-
-            else
-            {
-                return 0;
-            }
-        }
-
-        else
-        {
-            return 0;
-        }
+        if(_bossDefeated[bossName]) return 1;
+        else return 0;
     }
 #endregion
 
--- Assets/Scripts/StoreSceneScripts/BookActivation.cs
+++ Assets/Scripts/StoreSceneScripts/BookActivation.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Linq;
 using UnityEngine;
 
 
 [System.Serializable]
 // [CustomPropertyDrawer()]
-public class BookObjects : SerializableDictionary<string, GameObject[]>{}
+public class BookObjects : SerializableDictionary<string, List<GameObject>>{}
 public class BookActivation : MonoBehaviour
 {
+#region Book Objects
     [Header("Book Objects Dictionary")]
     public List<GameObject> PassiveBookObjects = new List<GameObject>();
     public BookObjects EuropeBookObjects = new BookObjects();
@@ -18,15 +20,9 @@ public class BookActivation : MonoBehaviour
     public BookObjects SouthAmericaBookObjects = new BookObjects();
     public BookObjects AfricaBookObjects = new BookObjects();
     public BookObjects AustraliaBookObjects = new BookObjects();
+#endregion
 
-    [Header("Boss List")]
-    public List<string> EuropeBoss = new List<string>();
-    public List<string> AsiaBoss = new List<string>();
-    public List<string> NorthAmericaBoss = new List<string>();
-    public List<string> SouthAmerica = new List<string>();
-    public List<string> AfricaBoss = new List<string>();
-    public List<string> AustraliaBoss = new List<string>();
-
+#region Legacy
     [Header("Legacy")]
     public GameObject[] Thor1;
     public GameObject[] Thor2;
@@ -51,161 +47,118 @@ public class BookActivation : MonoBehaviour
     public GameObject[] Cha;
     public GameObject[] Ten;
     public GameObject[] Ale;
+#endregion
 
     void Start()
     {
         ActivateBook();
-        EuropeBookObjects.Add("Thor", null);
-        EuropeBookObjects["Thor"] = new GameObject[100];
-        EuropeBookObjects.Add("Surtur", null);
+        ActivateBookObjects();
     }
 
+    private void ActivateBookObjects(){
+        foreach(string Boss in BookManager.Instance.EuropeBoss){
+            EuropeBookObjects.Add(Boss, null);
+            EuropeBookObjects[Boss] = new List<GameObject> {};
+        }
+        foreach(string Boss in BookManager.Instance.AsiaBoss){
+            AsiaBookObjects.Add(Boss, null);
+            AsiaBookObjects[Boss] = new List<GameObject> {};
+        }
+        foreach(string Boss in BookManager.Instance.NorthAmericaBoss){
+            NorthAmericaBookObjects.Add(Boss, null);
+            NorthAmericaBookObjects[Boss] = new List<GameObject> {};
+        }
+        foreach(string Boss in BookManager.Instance.SouthAmericaBoss){
+            SouthAmericaBookObjects.Add(Boss, null);
+            SouthAmericaBookObjects[Boss] = new List<GameObject> {};
+        }
+        foreach(string Boss in BookManager.Instance.AfricaBoss){
+            AfricaBookObjects.Add(Boss, null);
+            AfricaBookObjects[Boss] = new List<GameObject> {};
+        }
+        foreach(string Boss in BookManager.Instance.AustraliaBoss){
+            AustraliaBookObjects.Add(Boss, null);
+            AustraliaBookObjects[Boss] = new List<GameObject> {};
+        }
+    }
+
+    private GameObject location, bookObject;
     private void ActivateBook()
     {
-        switch (BookManager.Instance.CheckBookUnlocked("Thor1"))
-        {
-            case 0:
+        location = gameObject.transform.Find(BookManager.Instance.roomTypeSetting).gameObject;
+        switch (BookManager.Instance.roomTypeSetting){
+            case "Europe":
+                foreach(string Boss in BookManager.Instance.EuropeBoss){
+                    foreach(string Book in BookManager.Instance.EuropeBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-            case 1:
-                Thor1[0].gameObject.SetActive(true);
+            case "Asia":
+                foreach(string Boss in BookManager.Instance.AsiaBoss){
+                    foreach(string Book in BookManager.Instance.AsiaBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-            case 2:
-                Thor1[0].gameObject.SetActive(true);
-                Thor1[1].gameObject.SetActive(true);
+            case "NorthAmerica":
+                foreach(string Boss in BookManager.Instance.NorthAmericaBoss){
+                    foreach(string Book in BookManager.Instance.NorthAmericaBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Thor2"))
-        {
-            case 0:
+            case "SouthAmerica":
+                foreach(string Boss in BookManager.Instance.SouthAmericaBoss){
+                    foreach(string Book in BookManager.Instance.SouthAmericaBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-            case 1:
-                Thor2[0].gameObject.SetActive(true);
+            case "Africa":
+                foreach(string Boss in BookManager.Instance.AfricaBoss){
+                    foreach(string Book in BookManager.Instance.AfricaBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-            case 2:
-                Thor2[0].gameObject.SetActive(true);
-                Thor2[1].gameObject.SetActive(true);
+            case "Australia":
+                foreach(string Boss in BookManager.Instance.AustraliaBoss){
+                    foreach(string Book in BookManager.Instance.AustraliaBookList[Boss]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Thor3"))
-        {
-            case 0:
-                break;
-            case 1:
-                Thor3[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Thor3[0].gameObject.SetActive(true);
-                Thor3[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Thor4"))
-        {
-            case 0:
-                break;
-            case 1:
-                Thor4[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Thor4[0].gameObject.SetActive(true);
-                Thor4[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Surtr1"))
-        {
-            case 0:
-                break;
-            case 1:
-                Surtur1[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Surtur1[0].gameObject.SetActive(true);
-                Surtur1[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Surtr2"))
-        {
-            case 0:
-                break;
-            case 1:
-                Surtur2[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Surtur2[0].gameObject.SetActive(true);
-                Surtur2[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Surtr3"))
-        {
-            case 0:
-                break;
-            case 1:
-                Surtur3[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Surtur3[0].gameObject.SetActive(true);
-                Surtur3[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Surtr4"))
-        {
-            case 0:
-                break;
-            case 1:
-                Surtur4[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Surtur4[0].gameObject.SetActive(true);
-                Surtur4[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Challenge"))
-        {
-            case 0:
-                break;
-            case 1:
-                Cha[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Cha[0].gameObject.SetActive(true);
-                Cha[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Tenacity"))
-        {
-            case 0:
-                break;
-            case 1:
-                Ten[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Ten[0].gameObject.SetActive(true);
-                Ten[1].gameObject.SetActive(true);
-                break;
-        }
-
-        switch (BookManager.Instance.CheckBookUnlocked("Alertness"))
-        {
-            case 0:
-                break;
-            case 1:
-                Ale[0].gameObject.SetActive(true);
-                break;
-            case 2:
-                Ale[0].gameObject.SetActive(true);
-                Ale[1].gameObject.SetActive(true);
+            case "Passive":
+                foreach(string Passive in BookManager.Instance.PassiveList){
+                    foreach(string Book in BookManager.Instance.PassiveBookList[Passive]){
+                        bookObject = location.transform.Find(Book).gameObject;
+                        if(BookManager.Instance.CheckBookUnlocked(Book) > 0){
+                            bookObject.SetActive(true);
+                        }
+                    }
+                }
                 break;
         }
     }
-
 
 }
 
